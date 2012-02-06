ecaa0af9bbace82b213ac3f2434c7babeed3c236
Shirlee Berman <sberman@homefinder.com>
2012-02-05 21:46:18 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6a940e842ef9f4e74f5e485604b5128ea8f12c85917103ae45094cf682a97cb609a866807de58a982749dba9b1bcb3f97d04182a1e24120e337cdde3f6655699'
